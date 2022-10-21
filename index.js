6055c961280244dbc270319ec05eff101d212d23
Smz1n <samuel.gomesv2@gmail.com>
2022-10-21 09:35:31 -0300

    utilizando o morgan e o chalk

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express')
+const morgan = require('morgan')
+const chalk = require('chalk')
 const app = express()
 const port = 3000
 
 const produtoRota = require("./controllers/produto/router.js")
 
+const morganMiddleware = morgan(function (tokens, req, res) {
+  return [
+      '\n\n\n',
+      chalk.red(tokens.method(req, res)),
+      chalk.green(tokens.url(req, res)),
+      chalk.yellow(tokens['response-time'](req, res) + ' ms'),
+      '\n\n\n',
+  ].join(' ');
+});
+
+app.use(morganMiddleware);
+
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('A documentação da api')
 })
-
-app.use(express.json())
 
 app.use('/', produtoRota)
 
